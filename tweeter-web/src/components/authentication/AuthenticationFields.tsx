da5a57bf13da2f23e1c5b713d319ca33907a0c36
Tristan Weech <twminebro@gmail.com>
2026-06-23 08:55:02 -0600

    add(AuthenticationFields) props interface and functionality

--- tweeter-web/src/components/authentication/AuthenticationFields.tsx
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -1,3 +1,46 @@
-const AuthenticationFields = () => {};
+import { useState } from "react";
+
+interface Props {
+  doAuthentication: () => void;
+}
+
+const AuthenticationFields = (props: Props) => {
+  const [alias, setAlias] = useState("");
+  const [password, setPassword] = useState("");
+
+  const checkAuthentication = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.key == "Enter" && !!alias && !!password) {
+      props.doAuthentication();
+    }
+  };
+
+  return (
+    <>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          size={50}
+          id="aliasInput"
+          placeholder="name@example.com"
+          onKeyDown={checkAuthentication}
+          onChange={(event) => setAlias(event.target.value)}
+        />
+        <label htmlFor="aliasInput">Alias</label>
+      </div>
+      <div className="form-floating mb-3">
+        <input
+          type="password"
+          className="form-control bottom"
+          id="passwordInput"
+          placeholder="Password"
+          onKeyDown={checkAuthentication}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <label htmlFor="passwordInput">Password</label>
+      </div>
+    </>
+  );
+};
 
 export default AuthenticationFields;
