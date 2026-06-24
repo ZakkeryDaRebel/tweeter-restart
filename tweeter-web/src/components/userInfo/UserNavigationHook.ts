8d6a27ca69c80aca1e14d69490e9486a7901333a
Tristan Weech <twminebro@gmail.com>
2026-06-24 10:24:30 -0600

    add(UserNavigationHook) extractPath functionality

--- tweeter-web/src/components/userInfo/UserNavigationHook.ts
+++ tweeter-web/src/components/userInfo/UserNavigationHook.ts
@@ -36,7 +36,14 @@ export const useUserNavigation = () => {
   };
 
   const extractPath = (value: string): string => {
-    return value;
+    const index = value.indexOf("@");
+    console.log("Value: ", value);
+    const beforeIndex = value.substring(0, index - 1);
+    console.log("Before index: ", beforeIndex);
+    const slashIndex = beforeIndex.lastIndexOf("/");
+    const featurePath = beforeIndex.substring(slashIndex);
+    console.log("Feature path: ", featurePath);
+    return featurePath;
   };
 
   const getUser = async (
