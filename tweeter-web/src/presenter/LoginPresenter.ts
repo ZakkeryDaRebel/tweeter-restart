9c3505a06e1b4e28b72ac15131b1b9d4fe6cdbb3
Tristan Weech <twminebro@gmail.com>
2026-06-25 12:00:48 -0600

    add(LoginPresenter) functionality

--- tweeter-web/src/presenter/LoginPresenter.ts
+++ tweeter-web/src/presenter/LoginPresenter.ts
@@ -1,6 +1,20 @@
+import { User, AuthToken } from "tweeter-shared";
 import { UserService } from "../model.service/UserService";
 
-export interface LoginView {}
+export interface LoginView {
+  setIsLoading: (value: boolean) => void;
+  displayErrorMessage: (
+    message: string,
+    bootstrapClasses?: string | undefined,
+  ) => string;
+  updateUserInfo: (
+    currentUser: User,
+    displayedUser: User | null,
+    authToken: AuthToken,
+    remember: boolean,
+  ) => void;
+  navigate: (featureUrl: string) => void;
+}
 
 export class LoginPresenter {
   private service: UserService;
@@ -9,5 +23,32 @@ export class LoginPresenter {
   public constructor(view: LoginView) {
     this.view = view;
     this.service = new UserService();
+  }
+
+  public async doLogin(
+    alias: string,
+    password: string,
+    rememberMe: boolean,
+    originalUrl: string | undefined,
+  ) {
+    try {
+      this.view.setIsLoading(true);
+
+      const [user, authToken] = await this.service.login(alias, password);
+
+      this.view.updateUserInfo(user, user, authToken, rememberMe);
+
+      if (!!originalUrl) {
+        this.view.navigate(originalUrl);
+      } else {
+        this.view.navigate(`/feed/${user.alias}`);
+      }
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to log user in because of exception: ${error}`,
+      );
+    } finally {
+      this.view.setIsLoading(false);
+    }
   }
 }
