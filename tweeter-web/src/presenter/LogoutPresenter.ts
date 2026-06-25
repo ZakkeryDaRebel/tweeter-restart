f845fd71b6947397e9daa628c60681ef7f0f0ae9
Tristan Weech <twminebro@gmail.com>
2026-06-25 11:23:19 -0600

    add(LogoutPresenter)

---
+++ tweeter-web/src/presenter/LogoutPresenter.ts
@@ -0,0 +1,39 @@
+import { AuthToken } from "tweeter-shared";
+import { UserService } from "../model.service/UserService";
+
+export interface LogoutView {
+  displayInfoMessage: (message: string, duration: number) => string;
+  displayErrorMessage: (
+    message: string,
+    bootstrapClasses?: string | undefined,
+  ) => string;
+  deleteMessage: (messageId: string) => void;
+  clearUserInfo: () => void;
+  navigate: (featureUrl: string) => void;
+}
+
+export class LogoutPresenter {
+  private service: UserService;
+  private view: LogoutView;
+
+  public constructor(view: LogoutView) {
+    this.service = new UserService();
+    this.view = view;
+  }
+
+  public async logOut(authToken: AuthToken) {
+    const loggingOutToastId = this.view.displayInfoMessage("Logging Out...", 0);
+
+    try {
+      await this.service.logout(authToken!);
+
+      this.view.deleteMessage(loggingOutToastId);
+      this.view.clearUserInfo();
+      this.view.navigate("/login");
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to log user out because of exception: ${error}`,
+      );
+    }
+  }
+}
