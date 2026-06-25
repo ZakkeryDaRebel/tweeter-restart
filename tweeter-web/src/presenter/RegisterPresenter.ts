757dd5af433e653a7d4f94fe62ed1aec7afbf237
Tristan Weech <twminebro@gmail.com>
2026-06-25 12:05:16 -0600

    add(RegisterPresenter) variables and constructor

--- tweeter-web/src/presenter/RegisterPresenter.ts
+++ tweeter-web/src/presenter/RegisterPresenter.ts
@@ -1,3 +1,13 @@
+import { UserService } from "../model.service/UserService";
+
 export interface RegisterView {}
 
-export class RegisterPresenter {}
+export class RegisterPresenter {
+  private service: UserService;
+  private view: RegisterView;
+
+  public constructor(view: RegisterView) {
+    this.service = new UserService();
+    this.view = view;
+  }
+}
