fcce275e8b129d3aeaa74146d20b8f143fb15736
Tristan Weech <twminebro@gmail.com>
2026-06-29 10:50:57 -0600

    update(LogoutPresenter) to use doFailureReportingOperation

--- tweeter-web/src/presenter/LogoutPresenter.ts
+++ tweeter-web/src/presenter/LogoutPresenter.ts
@@ -19,17 +19,12 @@ export class LogoutPresenter extends Presenter<LogoutView> {
 
   public async logOut(authToken: AuthToken) {
     const loggingOutToastId = this.view.displayInfoMessage("Logging Out...", 0);
-
-    try {
+    this.doFailureReportingOperation(async () => {
       await this.service.logout(authToken!);
 
       this.view.deleteMessage(loggingOutToastId);
       this.view.clearUserInfo();
       this.view.navigate("/login");
-    } catch (error) {
-      this.view.displayErrorMessage(
-        `Failed to log user out because of exception: ${error}`,
-      );
-    }
+    }, "log user out");
   }
 }
