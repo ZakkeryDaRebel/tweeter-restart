8262825a02113dcd7040f2bd1b00949c0d8a31ba
Tristan Weech <twminebro@gmail.com>
2026-06-29 10:53:28 -0600

    update(PostStatusPresenter) to use doFailureAndFinallyReportingOperation

--- tweeter-web/src/presenter/PostStatusPresenter.ts
+++ tweeter-web/src/presenter/PostStatusPresenter.ts
@@ -23,27 +23,26 @@ export class PostStatusPresenter extends Presenter<PostStatusView> {
     post: string,
   ) {
     var postingStatusToastId = "";
+    this.doFailureAndFinallyReportingOperation(
+      async () => {
+        this.view.setIsLoading(true);
+        postingStatusToastId = this.view.displayInfoMessage(
+          "Posting status...",
+          0,
+        );
 
-    try {
-      this.view.setIsLoading(true);
-      postingStatusToastId = this.view.displayInfoMessage(
-        "Posting status...",
-        0,
-      );
+        const status = new Status(post, currentUser!, Date.now());
 
-      const status = new Status(post, currentUser!, Date.now());
+        await this.service.postStatus(authToken!, status);
 
-      await this.service.postStatus(authToken!, status);
-
-      this.view.setPost("");
-      this.view.displayInfoMessage("Status posted!", 2000);
-    } catch (error) {
-      this.view.displayErrorMessage(
-        `Failed to post the status because of exception: ${error}`,
-      );
-    } finally {
-      this.view.deleteMessage(postingStatusToastId);
-      this.view.setIsLoading(false);
-    }
+        this.view.setPost("");
+        this.view.displayInfoMessage("Status posted!", 2000);
+      },
+      "post the status",
+      () => {
+        this.view.deleteMessage(postingStatusToastId);
+        this.view.setIsLoading(false);
+      },
+    );
   }
 }
