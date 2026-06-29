c6f91af4d8e1d180cac7996b151b9f546b634bbf
Tristan Weech <twminebro@gmail.com>
2026-06-29 08:21:46 -0600

    add(UserNavigationPresenter)

---
+++ tweeter-web/src/presenter/UserNavigationPresenter.ts
@@ -0,0 +1,57 @@
+import { AuthToken, User } from "tweeter-shared";
+import { UserService } from "../model.service/UserService";
+
+export interface UserNavigationView {
+  setDisplayedUser: (user: User) => void;
+  navigate: (featureUrl: string) => void;
+  displayErrorMessage: (
+    message: string,
+    bootstrapClasses?: string | undefined,
+  ) => string;
+}
+
+export class UserNavigationPresenter {
+  private view: UserNavigationView;
+  private service: UserService;
+
+  public constructor(view: UserNavigationView) {
+    this.view = view;
+    this.service = new UserService();
+  }
+
+  public async navigateToUser(
+    eventString: string,
+    authToken: AuthToken,
+    displayedUser: User,
+  ): Promise<void> {
+    try {
+      const alias = this.extractAlias(eventString);
+      const featurePath = this.extractPath(eventString);
+
+      const toUser = await this.service.getUser(authToken, alias);
+
+      if (toUser) {
+        if (!toUser.equals(displayedUser!)) {
+          this.view.setDisplayedUser(toUser);
+          this.view.navigate(`${featurePath}/${toUser.alias}`);
+        }
+      }
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to get user because of exception: ${error}`,
+      );
+    }
+  }
+
+  public extractAlias(value: string): string {
+    const index = value.indexOf("@");
+    return value.substring(index);
+  }
+
+  public extractPath(value: string): string {
+    const index = value.indexOf("@");
+    const beforeIndex = value.substring(0, index - 1);
+    const slashIndex = beforeIndex.lastIndexOf("/");
+    return beforeIndex.substring(slashIndex);
+  }
+}
