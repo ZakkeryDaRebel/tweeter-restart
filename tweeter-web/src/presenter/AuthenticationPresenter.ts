433e805cec20c5723a661bd0405d8efe50c56659
Tristan Weech <twminebro@gmail.com>
2026-06-30 10:09:54 -0600

    add(AuthenticaitonPresenter) the abstract class to remove duplicate code between Login and Register

--- tweeter-web/src/presenter/AuthenticationPresenter.ts
+++ tweeter-web/src/presenter/AuthenticationPresenter.ts
@@ -1,3 +1,5 @@
+import { AuthToken, User } from "tweeter-shared";
+import { UserService } from "../model.service/UserService";
 import {
   LoadingView,
   NavigateView,
@@ -10,4 +12,68 @@ export interface AuthenticationView
 
 export abstract class AuthenticationPresenter<
   T extends AuthenticationView,
-> extends Presenter<T> {}
+> extends Presenter<T> {
+  private _rememberMe: boolean;
+  private _service: UserService;
+
+  public constructor(view: T) {
+    super(view);
+    this._rememberMe = false;
+    this._service = new UserService();
+  }
+
+  public get rememberMe() {
+    return this._rememberMe;
+  }
+
+  public set rememberMe(value: boolean) {
+    this._rememberMe = value;
+  }
+
+  public get service() {
+    return this._service;
+  }
+
+  public async doAuthentication(
+    alias: string,
+    password: string,
+    firstName?: string,
+    lastName?: string,
+  ) {
+    this.doFailureAndFinallyReportingOperation(
+      async () => {
+        this.view.setIsLoading(true);
+
+        const [user, authToken] = await this.authenticationOperation(
+          alias,
+          password,
+          firstName,
+          lastName,
+        );
+
+        this.view.updateUserInfo(user, user, authToken, this.rememberMe);
+
+        this.view.navigate(this.getDestinationUrl(user));
+      },
+      this.authenticationDescription(),
+      () => {
+        this.view.setIsLoading(false);
+      },
+    );
+  }
+
+  protected abstract authenticationOperation(
+    alias: string,
+    password: string,
+    firstName: string | undefined,
+    lastName: string | undefined,
+  ): Promise<[User, AuthToken]>;
+
+  protected abstract authenticationDescription(): string;
+
+  protected abstract getDestinationUrl(user: User): string;
+
+  protected defaultDestinationUrl(user: User): string {
+    return `/feed/${user.alias}`;
+  }
+}
