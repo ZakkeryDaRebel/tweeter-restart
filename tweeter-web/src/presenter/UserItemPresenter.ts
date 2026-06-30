8ea5116fb118df26cff02db4a7bc2458c7fed55a
Tristan Weech <twminebro@gmail.com>
2026-06-30 08:37:19 -0600

    update(UserItemPresenter) to implement PagedItemPresenter and vastly simplify the class

--- tweeter-web/src/presenter/UserItemPresenter.ts
+++ tweeter-web/src/presenter/UserItemPresenter.ts
@@ -1,43 +1,12 @@
-import { AuthToken, User } from "tweeter-shared";
-import { UserService } from "../model.service/UserService";
-import { Presenter, View } from "./Presenter";
+import { User } from "tweeter-shared";
+import { PagedItemPresenter } from "./PagedItemPresenter";
+import { FollowService } from "../model.service/FollowService";
 
-export interface UserItemView extends View {
-  addItems: (items: User[]) => void;
-}
-
-export abstract class UserItemPresenter extends Presenter<UserItemView> {
-  private _hasMoreItems = true;
-  private _lastItem: User | null = null;
-  private userService: UserService = new UserService();
-
-  protected get lastItem() {
-    return this._lastItem;
+export abstract class UserItemPresenter extends PagedItemPresenter<
+  User,
+  FollowService
+> {
+  protected serviceFactory(): FollowService {
+    return new FollowService();
   }
-
-  protected set lastItem(value: User | null) {
-    this._lastItem = value;
-  }
-
-  public get hasMoreItems() {
-    return this._hasMoreItems;
-  }
-
-  protected set hasMoreItems(value: boolean) {
-    this._hasMoreItems = value;
-  }
-
-  reset() {
-    this.lastItem = null;
-    this.hasMoreItems = true;
-  }
-
-  public async getUser(
-    authToken: AuthToken,
-    alias: string,
-  ): Promise<User | null> {
-    return await this.userService.getUser(authToken, alias);
-  }
-
-  public abstract loadMoreItems(authToken: AuthToken, userAlias: string): void;
 }
