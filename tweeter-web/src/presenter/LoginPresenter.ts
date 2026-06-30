b315aede31ec627f7a1d387d7c4920b064fad248
Tristan Weech <twminebro@gmail.com>
2026-06-30 09:02:53 -0600

    update(LoginPresenter) to extend AuthenticationPresenter instead of Presenter

--- tweeter-web/src/presenter/LoginPresenter.ts
+++ tweeter-web/src/presenter/LoginPresenter.ts
@@ -1,7 +1,10 @@
 import { UserService } from "../model.service/UserService";
-import { AuthenticationView, Presenter } from "./Presenter";
+import {
+  AuthenticationPresenter,
+  AuthenticationView,
+} from "./AuthenticationPresenter";
 
-export class LoginPresenter extends Presenter<AuthenticationView> {
+export class LoginPresenter extends AuthenticationPresenter<AuthenticationView> {
   private service: UserService;
 
   public constructor(view: AuthenticationView) {
