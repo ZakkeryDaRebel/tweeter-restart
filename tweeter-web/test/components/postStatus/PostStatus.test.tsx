33cf10e7d4943a25aa2fe48b0993494b31d1f777
Tristan Weech <twminebro@gmail.com>
2026-07-08 10:54:54 -0600

    add(PostStatus Test) enables both buttons when the text field has text

--- tweeter-web/test/components/postStatus/PostStatus.test.tsx
+++ tweeter-web/test/components/postStatus/PostStatus.test.tsx
@@ -3,6 +3,10 @@ import { MemoryRouter } from "react-router-dom";
 import PostStatus from "../../../src/components/postStatus/PostStatus";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import { useUserInfo } from "../../../src/components/userInfo/UserInfoHooks";
+import { AuthToken, User } from "tweeter-shared";
+
+const post: string = "Hello world!";
 
 describe("PostStatus Component", () => {
   it("starts with the post status and clear buttons disabled", async () => {
@@ -10,6 +14,16 @@ describe("PostStatus Component", () => {
 
     expect(postStatusButton).toBeDisabled();
     expect(clearButton).toBeDisabled();
+  });
+
+  it("enables both buttons when the text field has text", async () => {
+    const { postStatusButton, clearButton, user, textField } =
+      renderPostStatusAndGetElements();
+
+    await user.type(textField, post);
+
+    expect(postStatusButton).toBeEnabled();
+    expect(clearButton).toBeEnabled();
   });
 });
 
@@ -26,7 +40,7 @@ function renderPostStatusAndGetElements() {
 
   renderPostStatus();
 
-  const textField = screen.getByLabelText("text-field");
+  const textField = screen.getByLabelText("text");
   const postStatusButton = screen.getByRole("button", { name: /Post Status/ });
   const clearButton = screen.getByRole("button", { name: /Clear/ });
 
