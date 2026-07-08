e54eaf0b817522f4b973b5731ebbb97afeaaab5c
Tristan Weech <twminebro@gmail.com>
2026-07-08 08:53:01 -0600

    add(Login Test)

---
+++ tweeter-web/test/components/authentication/login/Login.test.tsx
@@ -0,0 +1,36 @@
+import { MemoryRouter } from "react-router-dom";
+import Login from "../../../../src/components/authentication/login/Login";
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(fab);
+
+describe("Login Component", () => {
+  it("starts with the sign-in button disabled", () => {
+    const { signInButton } = renderLoginAndGetElements("/");
+    expect(signInButton).toBeDisabled();
+  });
+});
+
+function renderLogin(originalUrl: string) {
+  return render(
+    <MemoryRouter>
+      <Login originalUrl={originalUrl}></Login>
+    </MemoryRouter>,
+  );
+}
+
+function renderLoginAndGetElements(originalUrl: string) {
+  const user = userEvent.setup();
+
+  renderLogin(originalUrl);
+
+  const signInButton = screen.getByRole("button", { name: /Sign in/i });
+  const aliasField = screen.getByLabelText("alias");
+  const passwordField = screen.getByLabelText("password");
+
+  return { user, signInButton, aliasField, passwordField };
+}
