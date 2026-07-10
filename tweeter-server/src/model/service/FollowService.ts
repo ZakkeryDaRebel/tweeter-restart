503a93e79b593a8667e387a23526800d6a2abdf2
Tristan Weech <twminebro@gmail.com>
2026-07-10 12:42:32 -0600

    update(Service) layer to be in model/service. Also updated FollowService

--- tweeter-server/src/model/service/FollowService.ts
+++ tweeter-server/src/model/service/FollowService.ts
@@ -1,25 +1,23 @@
-import { AuthToken, User, FakeData } from "tweeter-shared";
+import { AuthToken, User, FakeData, UserDto } from "tweeter-shared";
 import { Service } from "./Service";
 
 export class FollowService implements Service {
   public async loadMoreFollowees(
-    authToken: AuthToken,
+    token: string,
     userAlias: string,
     pageSize: number,
-    lastItem: User | null,
-  ): Promise<[User[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+    lastItem: UserDto | null,
+  ): Promise<[UserDto[], boolean]> {
+    return await this.getFakeData(lastItem, pageSize, userAlias);
   }
 
   public async loadMoreFollowers(
-    authToken: AuthToken,
+    token: string,
     userAlias: string,
     pageSize: number,
-    lastItem: User | null,
-  ): Promise<[User[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+    lastItem: UserDto | null,
+  ): Promise<[UserDto[], boolean]> {
+    return await this.getFakeData(lastItem, pageSize, userAlias);
   }
 
   public async getIsFollowerStatus(
@@ -81,5 +79,35 @@ export class FollowService implements Service {
     );
 
     return [followerCount, followeeCount];
+  }
+
+  private async getFakeData(
+    lastItem: UserDto | null,
+    pageSize: number,
+    userAlias: string,
+  ): Promise<[UserDto[], boolean]> {
+    // TODO: Replace with the result of calling server
+    const [items, hasMore] = FakeData.instance.getPageOfUsers(
+      this.getDomainObject(lastItem),
+      pageSize,
+      userAlias,
+    );
+    const dtos = items.map((user) => this.createDto(user));
+    return [dtos, hasMore];
+  }
+
+  private createDto(user: User): UserDto {
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      alias: user.alias,
+      imageUrl: user.imageUrl,
+    };
+  }
+
+  private getDomainObject(dto: UserDto | null): User | null {
+    return dto == null
+      ? null
+      : new User(dto.firstName, dto.lastName, dto.alias, dto.imageUrl);
   }
 }
