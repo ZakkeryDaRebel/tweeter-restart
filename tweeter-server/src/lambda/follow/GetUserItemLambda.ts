ba6e22e2d9a04a196d60846c9b525dbbbc47b333
Tristan Weech <twminebro@gmail.com>
2026-07-14 12:07:38 -0600

    update(GetUserItemLambda) to also remove duplicate code for the FollowService

--- tweeter-server/src/lambda/follow/GetUserItemLambda.ts
+++ tweeter-server/src/lambda/follow/GetUserItemLambda.ts
@@ -3,17 +3,21 @@ import {
   PagedUserItemResponse,
   UserDto,
 } from "tweeter-shared";
+import { FollowService } from "../../model/service/FollowService";
 
 export const handler = async (
   request: PagedUserItemRequest,
   serviceOperation: (
+    followService: FollowService,
     token: string,
     userAlias: string,
     pageSize: number,
     lastItem: UserDto | null,
   ) => Promise<[UserDto[], boolean]>,
 ): Promise<PagedUserItemResponse> => {
+  const followService = new FollowService();
   const [items, hasMore] = await serviceOperation(
+    followService,
     request.token,
     request.userAlias,
     request.pageSize,
