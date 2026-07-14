9f16a0f08f3e545b6be31b65ba53b337538bd066
Tristan Weech <twminebro@gmail.com>
2026-07-14 12:08:08 -0600

    update(GetFolloweesLambda) to utilize GetUserItemLambda

--- tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
+++ tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
@@ -1,20 +1,29 @@
-import { PagedUserItemRequest, PagedUserItemResponse } from "tweeter-shared";
+import {
+  PagedUserItemRequest,
+  PagedUserItemResponse,
+  UserDto,
+} from "tweeter-shared";
 import { FollowService } from "../../model/service/FollowService";
+import { handler as parentHandler } from "./GetUserItemLambda";
 
 export const handler = async (
   request: PagedUserItemRequest,
 ): Promise<PagedUserItemResponse> => {
-  const followService = new FollowService();
-  const [items, hasMore] = await followService.loadMoreFollowees(
-    request.token,
-    request.userAlias,
-    request.pageSize,
-    request.lastItem,
+  return await parentHandler(
+    request,
+    async (
+      followService: FollowService,
+      token: string,
+      userAlias: string,
+      pageSize: number,
+      lastItem: UserDto | null,
+    ): Promise<[UserDto[], boolean]> => {
+      return await followService.loadMoreFollowees(
+        token,
+        userAlias,
+        pageSize,
+        lastItem,
+      );
+    },
   );
-  return {
-    success: true,
-    message: null,
-    items: items,
-    hasMore: hasMore,
-  };
 };
