ccb3b2a8a28e35488a84751de419997a9df0ddb7
Tristan Weech <twminebro@gmail.com>
2026-07-15 12:57:58 -0600

    add(GetStatusItemLambda) functionality

--- tweeter-server/src/lambda/story/GetStatusItemLambda.ts
+++ tweeter-server/src/lambda/story/GetStatusItemLambda.ts
@@ -1,3 +1,33 @@
-import { PagedStatusItemRequest } from "tweeter-shared";
+import {
+  PagedStatusItemRequest,
+  PagedStatusItemResponse,
+  Status,
+  StatusDto,
+} from "tweeter-shared";
+import { StatusService } from "../../model/service/StatusService";
 
-export const handler = (request: PagedStatusItemRequest) => {};
+export const handler = async (
+  request: PagedStatusItemRequest,
+  serviceOperation: (
+    service: StatusService,
+    token: string,
+    userAlias: string,
+    pageSize: number,
+    lastItem: StatusDto | null,
+  ) => Promise<[StatusDto[], boolean]>,
+): Promise<PagedStatusItemResponse> => {
+  const statusService: StatusService = new StatusService();
+  const [items, hasMore] = await serviceOperation(
+    statusService,
+    request.token,
+    request.userAlias,
+    request.pageSize,
+    request.lastItem,
+  );
+  return {
+    success: true,
+    message: null,
+    items: items,
+    hasMore: hasMore,
+  };
+};
