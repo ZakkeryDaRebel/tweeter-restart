389c88662a495fdbf90e50a1c24df64d5f65b445
Tristan Weech <twminebro@gmail.com>
2026-07-16 12:56:53 -0600

    add(UnfollowLambda)

---
+++ tweeter-server/src/lambda/follow/command/UnfollowLambda.ts
@@ -0,0 +1,18 @@
+import { FollowCommandRequest, FollowCommandResponse } from "tweeter-shared";
+import { FollowService } from "../../../model/service/FollowService";
+
+export const handler = async (
+  request: FollowCommandRequest,
+): Promise<FollowCommandResponse> => {
+  const followService = new FollowService();
+  const [followerCount, followeeCount] = await followService.unfollow(
+    request.token,
+    request.userToInteract,
+  );
+  return {
+    success: true,
+    message: null,
+    followerCount: followerCount,
+    followeeCount: followeeCount,
+  };
+};
