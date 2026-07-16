f195e00426c0fa9058f1d309a68d4d60bd3849e5
Tristan Weech <twminebro@gmail.com>
2026-07-16 12:55:53 -0600

    add(FollowLambda) functionality

--- tweeter-server/src/lambda/follow/command/FollowLambda.ts
+++ tweeter-server/src/lambda/follow/command/FollowLambda.ts
@@ -1,1 +1,18 @@
-export const handler = async () => {};
+import { FollowCommandRequest, FollowCommandResponse } from "tweeter-shared";
+import { FollowService } from "../../../model/service/FollowService";
+
+export const handler = async (
+  request: FollowCommandRequest,
+): Promise<FollowCommandResponse> => {
+  const followService = new FollowService();
+  const [followerCount, followeeCount] = await followService.follow(
+    request.token,
+    request.userToInteract,
+  );
+  return {
+    success: true,
+    message: null,
+    followerCount: followerCount,
+    followeeCount: followeeCount,
+  };
+};
