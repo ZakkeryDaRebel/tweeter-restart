17f1d9fb94dc2b6d93d49a0f47f3f6e266936849
Tristan Weech <twminebro@gmail.com>
2026-07-16 11:30:57 -0600

    update(IsFollowerRequest) to extend TweeterRequest

--- tweeter-shared/src/model/net/request/IsFollowerRequest.ts
+++ tweeter-shared/src/model/net/request/IsFollowerRequest.ts
@@ -1,6 +1,7 @@
 import { UserDto } from "../../dto/UserDto";
+import { TweeterRequest } from "./TweeterReqest";
 
-export interface IsFollowerRequest {
+export interface IsFollowerRequest extends TweeterRequest {
   token: string;
   user: UserDto;
   selectedUser: UserDto;
