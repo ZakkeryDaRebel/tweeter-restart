86baba11ea752a4290b5048c63b5244247a8d2ad
Tristan Weech <twminebro@gmail.com>
2026-07-17 09:39:54 -0600

    add(PostStatusLambda) functionality

--- tweeter-server/src/lambda/story/PostStatusLambda.ts
+++ tweeter-server/src/lambda/story/PostStatusLambda.ts
@@ -1,1 +1,13 @@
-export const handler = async () => {};
+import { PostStatusRequest, TweeterResponse } from "tweeter-shared";
+import { StatusService } from "../../model/service/StatusService";
+
+export const handler = async (
+  request: PostStatusRequest,
+): Promise<TweeterResponse> => {
+  const statusService = new StatusService();
+  await statusService.postStatus(request.token, request.newStatus);
+  return {
+    success: true,
+    message: null,
+  };
+};
