fe58b1eeddf9c995babb7ad0573031862c93c581
Tristan Weech <twminebro@gmail.com>
2026-07-17 09:57:31 -0600

    update(UserService) to use token strin and return UserDto or null

--- tweeter-server/src/model/service/UserService.ts
+++ tweeter-server/src/model/service/UserService.ts
@@ -1,14 +1,12 @@
 import { Buffer } from "buffer";
-import { AuthToken, User, FakeData } from "tweeter-shared";
+import { AuthToken, User, FakeData, UserDto } from "tweeter-shared";
 import { Service } from "./Service";
 
 export class UserService implements Service {
-  public async getUser(
-    authToken: AuthToken,
-    alias: string,
-  ): Promise<User | null> {
+  public async getUser(token: string, alias: string): Promise<UserDto | null> {
     // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
+    const dbUser: User | null = FakeData.instance.findUserByAlias(alias);
+    return dbUser == null ? null : dbUser.getDto();
   }
 
   public async logout(authToken: AuthToken): Promise<void> {
