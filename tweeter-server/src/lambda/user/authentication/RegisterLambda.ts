fd461d85e31215c11c04439e69ca0ac799579316
Tristan Weech <twminebro@gmail.com>
2026-07-20 10:19:18 -0600

    remove(RegisterLambda) unnecessary imports

--- tweeter-server/src/lambda/user/authentication/RegisterLambda.ts
+++ tweeter-server/src/lambda/user/authentication/RegisterLambda.ts
@@ -1,5 +1,4 @@
 import { RegisterRequest, SignInResponse } from "tweeter-shared";
-import { UserService } from "../../../model/service/UserService";
 import { handler as parentHandler } from "./AuthenticationLambda";
 
 export const handler = async (
