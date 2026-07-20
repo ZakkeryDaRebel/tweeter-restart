a884712eb3210bafb22133200214aae7b60ab2bc
Tristan Weech <twminebro@gmail.com>
2026-07-20 10:17:13 -0600

    update(RegisterLambda) to use AuthenticationLambda

--- tweeter-server/src/lambda/user/authentication/RegisterLambda.ts
+++ tweeter-server/src/lambda/user/authentication/RegisterLambda.ts
@@ -1,22 +1,19 @@
 import { RegisterRequest, SignInResponse } from "tweeter-shared";
 import { UserService } from "../../../model/service/UserService";
+import { handler as parentHandler } from "./AuthenticationLambda";
 
 export const handler = async (
   request: RegisterRequest,
 ): Promise<SignInResponse> => {
-  const service = new UserService();
-  const [user, authToken] = await service.register(
-    request.firstName,
-    request.lastName,
-    request.alias,
-    request.password,
-    request.imageStringBase64,
-    request.imageFileExtension,
+  return await parentHandler(
+    async (service) =>
+      await service.register(
+        request.firstName,
+        request.lastName,
+        request.alias,
+        request.password,
+        request.imageStringBase64,
+        request.imageFileExtension,
+      ),
   );
-  return {
-    success: true,
-    message: null,
-    user: user,
-    authToken: authToken,
-  };
 };
