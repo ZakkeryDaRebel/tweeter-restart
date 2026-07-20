f8fc88ec2e0bbdae36e2e6aff5ab3e0f62a24b6f
Tristan Weech <twminebro@gmail.com>
2026-07-20 09:18:09 -0600

    remove(FollowCommandRequest) unused import

--- tweeter-shared/src/model/net/request/FollowCommandRequest.ts
+++ tweeter-shared/src/model/net/request/FollowCommandRequest.ts
@@ -1,6 +1,5 @@
 import { UserDto } from "../../dto/UserDto";
 import { AuthenticatedRequest } from "./AuthenticatedRequest";
-import { TweeterRequest } from "./TweeterReqest";
 
 export interface FollowCommandRequest extends AuthenticatedRequest {
   userToInteract: UserDto;
