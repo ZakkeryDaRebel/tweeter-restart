0fa63c8b0b2db3152bbded5a1be94152662ae39e
Tristan Weech <twminebro@gmail.com>
2026-07-21 12:08:45 -0600

    update(IsFollowerRequest) to use userAlias instead of userDto

--- tweeter-shared/src/model/net/request/IsFollowerRequest.ts
+++ tweeter-shared/src/model/net/request/IsFollowerRequest.ts
@@ -1,7 +1,6 @@
-import { UserDto } from "../../dto/UserDto";
 import { AuthenticatedRequest } from "./AuthenticatedRequest";
 
 export interface IsFollowerRequest extends AuthenticatedRequest {
-  user: UserDto;
-  selectedUser: UserDto;
+  userAlias: string;
+  selectedUserAlias: string;
 }
