c4c5e3b90600dbbac94e2f9cc2b7f965743ee97f
Tristan Weech <twminebro@gmail.com>
2026-07-21 12:08:09 -0600

    update(GetFollowCountRequest) to use userAlias instead of userDto

--- tweeter-shared/src/model/net/request/GetFollowCountRequest.ts
+++ tweeter-shared/src/model/net/request/GetFollowCountRequest.ts
@@ -1,6 +1,5 @@
-import { UserDto } from "../../dto/UserDto";
 import { AuthenticatedRequest } from "./AuthenticatedRequest";
 
 export interface GetFollowCountRequest extends AuthenticatedRequest {
-  user: UserDto;
+  userAlias: string;
 }
