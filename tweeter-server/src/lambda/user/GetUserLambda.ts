29ece9ef8bb24302a7ead25b5d9881d71e690358
Tristan Weech <twminebro@gmail.com>
2026-07-22 08:56:48 -0600

    update(GetUserLambda) to use TokenedAliasRequest

--- tweeter-server/src/lambda/user/GetUserLambda.ts
+++ tweeter-server/src/lambda/user/GetUserLambda.ts
@@ -1,11 +1,11 @@
-import { GetUserRequest, GetUserResponse } from "tweeter-shared";
+import { GetUserResponse, TokenedAliasRequest } from "tweeter-shared";
 import { UserService } from "../../model/service/UserService";
 
 export const handler = async (
-  request: GetUserRequest,
+  request: TokenedAliasRequest,
 ): Promise<GetUserResponse> => {
   const service = new UserService();
-  const user = await service.getUser(request.token, request.alias);
+  const user = await service.getUser(request.token, request.userAlias);
   return {
     success: true,
     message: null,
