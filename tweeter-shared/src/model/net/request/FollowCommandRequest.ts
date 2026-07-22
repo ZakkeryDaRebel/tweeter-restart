01392b898dc327244929889c4bf5c939c24fc1e1
Tristan Weech <twminebro@gmail.com>
2026-07-22 08:14:32 -0600

    delete(FollowCommandRequest) and replace with TokenedAliasRequest

--- tweeter-shared/src/model/net/request/FollowCommandRequest.ts
+++
@@ -1,5 +0,0 @@
-import { AliasRequest } from "./AliasRequest";
-import { AuthenticatedRequest } from "./AuthenticatedRequest";
-
-export interface FollowCommandRequest
-  extends AuthenticatedRequest, AliasRequest {}
