bc9b339a8f502ac34878f66b2f6d3c3ff1a196e0
Tristan Weech <twminebro@gmail.com>
2026-07-23 10:37:50 -0600

    update(PostStatusLambda) to use ServiceFactory

--- tweeter-server/src/lambda/status/PostStatusLambda.ts
+++ tweeter-server/src/lambda/status/PostStatusLambda.ts
@@ -1,11 +1,10 @@
 import { PostStatusRequest, TweeterResponse } from "tweeter-shared";
-import { StatusService } from "../../model/service/StatusService";
+import { ServiceFactory } from "../factory/ServiceFactory";
 
 export const handler = async (
   request: PostStatusRequest,
 ): Promise<TweeterResponse> => {
-  const statusService = new StatusService();
-  await statusService.postStatus(
+  await ServiceFactory.statusService.postStatus(
     request.token,
     request.userAlias,
     request.post,
