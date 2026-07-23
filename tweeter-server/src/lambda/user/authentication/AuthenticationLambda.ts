04be508efa97597d9f5996badc4bc963c927329e
Tristan Weech <twminebro@gmail.com>
2026-07-23 10:38:20 -0600

    update(AuthenticationLambda) to use ServiceFactory

--- tweeter-server/src/lambda/user/authentication/AuthenticationLambda.ts
+++ tweeter-server/src/lambda/user/authentication/AuthenticationLambda.ts
@@ -1,11 +1,11 @@
 import { AuthToken, SignInResponse, UserDto } from "tweeter-shared";
 import { UserService } from "../../../model/service/UserService";
+import { ServiceFactory } from "../../factory/ServiceFactory";
 
 export const handler = async (
   serviceOperation: (service: UserService) => Promise<[UserDto, AuthToken]>,
 ): Promise<SignInResponse> => {
-  const service = new UserService();
-  const [user, authToken] = await serviceOperation(service);
+  const [user, authToken] = await serviceOperation(ServiceFactory.userService);
   return {
     success: true,
     message: null,
