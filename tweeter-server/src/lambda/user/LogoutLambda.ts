d4b2fb9e9a27d168bd971e149a0b032afee89225
Tristan Weech <twminebro@gmail.com>
2026-07-23 10:39:21 -0600

    update(LogoutLambda) to use ServiceFactory

--- tweeter-server/src/lambda/user/LogoutLambda.ts
+++ tweeter-server/src/lambda/user/LogoutLambda.ts
@@ -1,11 +1,10 @@
 import { AuthenticatedRequest, TweeterResponse } from "tweeter-shared";
-import { UserService } from "../../model/service/UserService";
+import { ServiceFactory } from "../factory/ServiceFactory";
 
 export const handler = async (
   request: AuthenticatedRequest,
 ): Promise<TweeterResponse> => {
-  const service = new UserService();
-  await service.logout(request.token);
+  await ServiceFactory.userService.logout(request.token);
   return {
     success: true,
     message: null,
