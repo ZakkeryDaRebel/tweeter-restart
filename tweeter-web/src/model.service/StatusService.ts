25e27ddea44932fdca141a29a54c18bc2683bebc
Tristan Weech <twminebro@gmail.com>
2026-07-23 12:18:46 -0600

    update(StatusService) to call getMoreStatusItems

--- tweeter-web/src/model.service/StatusService.ts
+++ tweeter-web/src/model.service/StatusService.ts
@@ -13,8 +13,15 @@ export class StatusService extends Service {
     pageSize: number,
     lastItem: Status | null,
   ): Promise<[Status[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+    return await this.getMoreStatusItems(
+      authToken.token,
+      userAlias,
+      pageSize,
+      lastItem,
+      async (request: PagedStatusItemRequest): Promise<[Status[], boolean]> => {
+        return await this.serverFacade.getMoreFeed(request);
+      },
+    );
   }
 
   public async loadMoreStoryItems(
