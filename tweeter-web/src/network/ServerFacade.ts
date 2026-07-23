4af12144395d6eeeb4b77537f263a8df17f057c8
Tristan Weech <twminebro@gmail.com>
2026-07-23 11:39:16 -0600

    update(ServerFacade) with getMoreUserItems, getMoreItems, and send helper methods

--- tweeter-web/src/network/ServerFacade.ts
+++ tweeter-web/src/network/ServerFacade.ts
@@ -1,6 +1,10 @@
 import {
+  PagedItemRequest,
+  PagedItemResponse,
   PagedUserItemRequest,
   PagedUserItemResponse,
+  TweeterRequest,
+  TweeterResponse,
   User,
   UserDto,
 } from "tweeter-shared";
@@ -11,6 +15,10 @@ export class ServerFacade {
     "https://pesd1jjid6.execute-api.us-east-1.amazonaws.com/prod";
 
   private clientCommunicator = new ClientCommunicator(this.SERVER_URL);
+
+  //
+  // FollowService Methods
+  //
 
   public async getMoreFollowees(
     request: PagedUserItemRequest,
@@ -28,24 +36,61 @@ export class ServerFacade {
     req: PagedUserItemRequest,
     urlPath: string,
   ): Promise<[User[], boolean]> {
-    const response = await this.clientCommunicator.doPost<
+    return await this.getMoreItems<
       PagedUserItemRequest,
+      User,
+      UserDto,
       PagedUserItemResponse
-    >(req, urlPath);
+    >(
+      req,
+      urlPath,
+      (items: UserDto[]) => {
+        return items.map((dto) => User.fromDto(dto) as User);
+      },
+      "followees",
+    );
+  }
 
-    // Convert the UserDto array returned by ClientCommunicator to a User array
-    const items: User[] | null =
-      response.success && response.items
-        ? response.items.map((dto) => User.fromDto(dto) as User)
+  //
+  // Helper Methods
+  //
+
+  public async getMoreItems<
+    T extends PagedItemRequest<D>,
+    R,
+    D,
+    V extends PagedItemResponse<D>,
+  >(
+    request: T,
+    endpoint: string,
+    convertOperation: (items: D[]) => R[] | null,
+    itemDescription: string,
+  ): Promise<[R[], boolean]> {
+    return await this.send(request, endpoint, (response: V) => {
+      const items: R[] | null = response.items
+        ? convertOperation(response.items)
         : null;
 
-    // Handle errors
-    if (response.success) {
       if (items == null) {
-        throw new Error(`No followees found`);
+        throw new Error(`No ${itemDescription} found`);
       } else {
         return [items, response.hasMore];
       }
+    });
+  }
+
+  public async send<T extends TweeterRequest, R extends TweeterResponse, S>(
+    request: T,
+    endpoint: string,
+    onSuccess: (response: R) => S,
+  ): Promise<S> {
+    const response = await this.clientCommunicator.doPost<T, R>(
+      request,
+      endpoint,
+    );
+
+    if (response.success) {
+      return onSuccess(response);
     } else {
       console.error(response);
       throw new Error(response.message ?? undefined);
