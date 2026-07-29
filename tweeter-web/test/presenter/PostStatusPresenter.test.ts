cc8e13ceff4fc3ad9d23d8a6b921b63b4e1bc837
Tristan Weech <twminebro@gmail.com>
2026-07-29 15:05:31 -0600

    update(PostStatusPresenter Test) because it doesn't need to be commented out

--- tweeter-web/test/presenter/PostStatusPresenter.test.ts
+++ tweeter-web/test/presenter/PostStatusPresenter.test.ts
@@ -1,103 +1,101 @@
-// import {
-//   anything,
-//   instance,
-//   mock,
-//   spy,
-//   verify,
-//   when,
-//   capture,
-// } from "@typestrong/ts-mockito";
-// import {
-//   PostStatusPresenter,
-//   PostStatusView,
-// } from "../../src/presenter/PostStatusPresenter";
-// import { AuthToken, User } from "tweeter-shared";
-// import { StatusService } from "../../src/model.service/StatusService";
+import {
+  anything,
+  instance,
+  mock,
+  spy,
+  verify,
+  when,
+  capture,
+} from "@typestrong/ts-mockito";
+import {
+  PostStatusPresenter,
+  PostStatusView,
+} from "../../src/presenter/PostStatusPresenter";
+import { AuthToken, User } from "tweeter-shared";
+import { StatusService } from "../../src/model.service/StatusService";
 
-// const post = "Hello world!";
-// const user: User = new User(
-//   "Bob",
-//   "Billby",
-//   "@Bob-Billbly",
-//   "image-url-not-found",
-// );
-// const authToken: AuthToken = new AuthToken("token", Date.now());
-// const messageId = "messageId1234";
+const post = "Hello world!";
+const user: User = new User(
+  "Bob",
+  "Billby",
+  "@Bob-Billbly",
+  "image-url-not-found",
+);
+const authToken: AuthToken = new AuthToken("token", Date.now());
+const messageId = "messageId1234";
 
-// describe("PostStatusPresenter", () => {
-//   let mockPostStatusPresenterView: PostStatusView;
-//   let postStatusPresenter: PostStatusPresenter;
-//   let mockService: StatusService;
+describe("PostStatusPresenter", () => {
+  let mockPostStatusPresenterView: PostStatusView;
+  let postStatusPresenter: PostStatusPresenter;
+  let mockService: StatusService;
 
-//   beforeEach(() => {
-//     mockPostStatusPresenterView = mock<PostStatusView>();
-//     const mockPostPresenterViewInstance = instance(mockPostStatusPresenterView);
-//     when(
-//       mockPostStatusPresenterView.displayInfoMessage(anything(), 0),
-//     ).thenReturn(messageId);
+  beforeEach(() => {
+    mockPostStatusPresenterView = mock<PostStatusView>();
+    const mockPostPresenterViewInstance = instance(mockPostStatusPresenterView);
+    when(
+      mockPostStatusPresenterView.displayInfoMessage(anything(), 0),
+    ).thenReturn(messageId);
 
-//     const postStatusPresenterSpy = spy(
-//       new PostStatusPresenter(mockPostPresenterViewInstance),
-//     );
-//     postStatusPresenter = instance(postStatusPresenterSpy);
+    const postStatusPresenterSpy = spy(
+      new PostStatusPresenter(mockPostPresenterViewInstance),
+    );
+    postStatusPresenter = instance(postStatusPresenterSpy);
 
-//     mockService = mock<StatusService>();
-//     Object.defineProperty(postStatusPresenter, "service", {
-//       get: () => instance(mockService),
-//     });
-//   });
+    mockService = mock<StatusService>();
+    Object.defineProperty(postStatusPresenter, "service", {
+      get: () => instance(mockService),
+    });
+  });
 
-//   it("tells the view to display a posting status message.", async () => {
-//     await postStatusPresenter.submitPost(user, authToken, post);
+  it("tells the view to display a posting status message.", async () => {
+    await postStatusPresenter.submitPost(user, authToken, post);
 
-//     verify(
-//       mockPostStatusPresenterView.displayInfoMessage("Posting status...", 0),
-//     ).once();
-//   });
+    verify(
+      mockPostStatusPresenterView.displayInfoMessage("Posting status...", 0),
+    ).once();
+  });
 
-//   it("calls postStatus on the post status service with the correct status string and auth token.", async () => {
-//     await postStatusPresenter.submitPost(user, authToken, post);
+  it("calls postStatus on the post status service with the correct status string and auth token.", async () => {
+    await postStatusPresenter.submitPost(user, authToken, post);
 
-//     verify(mockService.postStatus(authToken, anything())).once();
-//     let [, status] = capture(mockService.postStatus).last();
-//     expect(status.post).toEqual(post);
-//   });
+    verify(mockService.postStatus(authToken, anything())).once();
+    let [, status] = capture(mockService.postStatus).last();
+    expect(status.post).toEqual(post);
+  });
 
-//   it("tells the view to clear the info message that was displayed previously, clear the post, and display a status posted message when successfull", async () => {
-//     await postStatusPresenter.submitPost(user, authToken, post);
+  it("tells the view to clear the info message that was displayed previously, clear the post, and display a status posted message when successfull", async () => {
+    await postStatusPresenter.submitPost(user, authToken, post);
 
-//     verify(mockPostStatusPresenterView.deleteMessage(messageId)).once();
-//     verify(mockPostStatusPresenterView.setPost("")).once();
-//     verify(
-//       mockPostStatusPresenterView.displayInfoMessage("Status posted!", 2000),
-//     ).once();
+    verify(mockPostStatusPresenterView.deleteMessage(messageId)).once();
+    verify(mockPostStatusPresenterView.setPost("")).once();
+    verify(
+      mockPostStatusPresenterView.displayInfoMessage("Status posted!", 2000),
+    ).once();
 
-//     verify(
-//       mockPostStatusPresenterView.displayErrorMessage(
-//         "Failed to post the status because of exception: An error occurred",
-//       ),
-//     ).never();
-//   });
+    verify(
+      mockPostStatusPresenterView.displayErrorMessage(
+        "Failed to post the status because of exception: An error occurred",
+      ),
+    ).never();
+  });
 
-//   it("tells the view to clear the info message and display an error message but does not tell it to clear the post or display a status posted message when unsuccessful", async () => {
-//     when(mockService.postStatus(anything(), anything())).thenThrow(
-//       new Error("An error occurred"),
-//     );
+  it("tells the view to clear the info message and display an error message but does not tell it to clear the post or display a status posted message when unsuccessful", async () => {
+    when(mockService.postStatus(anything(), anything())).thenThrow(
+      new Error("An error occurred"),
+    );
 
-//     await postStatusPresenter.submitPost(user, authToken, post);
+    await postStatusPresenter.submitPost(user, authToken, post);
 
-//     verify(mockPostStatusPresenterView.deleteMessage(messageId)).once();
-//     verify(
-//       mockPostStatusPresenterView.displayErrorMessage(
-//         "Failed to post the status because of exception: An error occurred",
-//       ),
-//     ).once();
+    verify(mockPostStatusPresenterView.deleteMessage(messageId)).once();
+    verify(
+      mockPostStatusPresenterView.displayErrorMessage(
+        "Failed to post the status because of exception: An error occurred",
+      ),
+    ).once();
 
-//     verify(mockPostStatusPresenterView.setPost("")).never();
-//     verify(
-//       mockPostStatusPresenterView.displayInfoMessage("Status posted!", 2000),
-//     ).never();
-//   });
-// });
-
-it("ignore PostStatusPresenter", () => {});
+    verify(mockPostStatusPresenterView.setPost("")).never();
+    verify(
+      mockPostStatusPresenterView.displayInfoMessage("Status posted!", 2000),
+    ).never();
+  });
+});
