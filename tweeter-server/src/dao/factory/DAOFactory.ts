afa86af39b538d70eb12b23d6649e15f1c8bdf6a
Tristan Weech <twminebro@gmail.com>
2026-07-30 15:06:41 -0600

    add(DAOFactory) auth, follow, image, and story DAOs

--- tweeter-server/src/dao/factory/DAOFactory.ts
+++ tweeter-server/src/dao/factory/DAOFactory.ts
@@ -1,7 +1,15 @@
+import { AuthDAO } from "../auth/AuthDAO";
+import { FollowDAO } from "../follow/FollowDAO";
+import { ImageDAO } from "../image/ImageDAO";
+import { StoryDAO } from "../story/StoryDAO";
 import { UserDAO } from "../user/UserDAO";
 
 export class DAOFactory {
   private constructor() {}
 
+  public static readonly authDAO: AuthDAO;
+  public static readonly followDAO: FollowDAO;
+  public static readonly imageDAO: ImageDAO;
+  public static readonly storyDAO: StoryDAO;
   public static readonly userDAO: UserDAO;
 }
