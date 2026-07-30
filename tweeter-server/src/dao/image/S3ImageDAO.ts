3d549121feb6ae10eb198175caa3fbae1c09527f
Tristan Weech <twminebro@gmail.com>
2026-07-30 12:58:42 -0600

    add(S3ImageDAO) stub

---
+++ tweeter-server/src/dao/image/S3ImageDAO.ts
@@ -0,0 +1,8 @@
+import { ImageDAO } from "./ImageDAO";
+
+export class S3ImageDAO implements ImageDAO {
+  createImage(imageStringBase64: string, imageFileExtension: string): string {
+    //code
+    return "";
+  }
+}
