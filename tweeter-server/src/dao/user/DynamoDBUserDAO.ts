3456a02e961b40f2d0ac3c15d95867ab649a265c
Tristan Weech <twminebro@gmail.com>
2026-07-30 12:54:36 -0600

    update(DynamoDBUserDAO) comment about DynamoDB table setup

--- tweeter-server/src/dao/user/DynamoDBUserDAO.ts
+++ tweeter-server/src/dao/user/DynamoDBUserDAO.ts
@@ -1,6 +1,15 @@
 import { User } from "tweeter-shared";
 import { UserDAO } from "./UserDAO";
 
+/*
+ * User Table Description
+ * Hash -> userAlias: string
+ * firstName: string
+ * lastName: string
+ * password: string (hashed)
+ * imageUrl: string (reference to S3 Bucket)
+ */
+
 export class DynamoDBUserDAO implements UserDAO {
   createUser(user: User): void {
     //code
