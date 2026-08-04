3a8df783ce5868d90af6b8e0671ab90b659b7b9b
Tristan Weech <twminebro@gmail.com>
2026-08-04 12:51:17 -0600

    add(DAOFactory) dynamoDBClient

--- tweeter-server/src/dao/factory/DAOFactory.ts
+++ tweeter-server/src/dao/factory/DAOFactory.ts
@@ -3,9 +3,21 @@ import { FollowDAO } from "../follow/FollowDAO";
 import { ImageDAO } from "../image/ImageDAO";
 import { StoryDAO } from "../story/StoryDAO";
 import { UserDAO } from "../user/UserDAO";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  QueryCommand,
+  UpdateCommand,
+} from "@aws-sdk/lib-dynamodb";
 
 export class DAOFactory {
   private constructor() {}
+  public static readonly dynamoDBClient = DynamoDBDocumentClient.from(
+    new DynamoDBClient(),
+  );
 
   public static readonly authDAO: AuthDAO;
   public static readonly followDAO: FollowDAO;
