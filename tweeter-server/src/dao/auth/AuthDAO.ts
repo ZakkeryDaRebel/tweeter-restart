f417ecc257595231f1ff119989a7e457a8e9b3c3
Tristan Weech <twminebro@gmail.com>
2026-08-05 09:38:43 -0600

    update(AuthDAO) to have all return values be a Promise

--- tweeter-server/src/dao/auth/AuthDAO.ts
+++ tweeter-server/src/dao/auth/AuthDAO.ts
@@ -1,8 +1,8 @@
 import { AuthToken } from "tweeter-shared";
 
 export interface AuthDAO {
-  createAuth(authToken: AuthToken, userAlias: string): void;
-  getAuth(token: string): [authToken: AuthToken, userAlias: string];
-  updateAuth(authToken: AuthToken): void;
-  deleteAuth(token: string): void;
+  createAuth(authToken: AuthToken, userAlias: string): Promise<void>;
+  getAuth(token: string): Promise<[authToken: AuthToken, userAlias: string]>;
+  updateAuth(authToken: AuthToken): Promise<void>;
+  deleteAuth(token: string): Promise<void>;
 }
