899799ad27a58c6756fd633ce13371a8067f721b
Tristan Weech <twminebro@gmail.com>
2026-08-05 08:49:54 -0600

    add(DynamoDBAuthDAO) method stubs

---
+++ tweeter-server/src/dao/auth/DynamoDBAuthDAO.ts
@@ -0,0 +1,23 @@
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { AuthToken } from "tweeter-shared";
+import { AuthDAO } from "./AuthDAO";
+
+export class DynamoDBAuthDAO implements AuthDAO {
+  private readonly client: DynamoDBDocumentClient;
+
+  public constructor(client: DynamoDBDocumentClient) {
+    this.client = client;
+  }
+  createAuth(authToken: AuthToken, userAlias: string): void {
+    throw new Error("Method not implemented.");
+  }
+  getAuth(token: string): [authToken: AuthToken, userAlias: string] {
+    throw new Error("Method not implemented.");
+  }
+  updateAuth(authToken: AuthToken): void {
+    throw new Error("Method not implemented.");
+  }
+  deleteAuth(token: string): void {
+    throw new Error("Method not implemented.");
+  }
+}
