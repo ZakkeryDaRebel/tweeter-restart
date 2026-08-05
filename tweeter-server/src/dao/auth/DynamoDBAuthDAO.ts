509150339fdb60bd42beeba6988630496b1ca48d
Tristan Weech <twminebro@gmail.com>
2026-08-05 09:12:23 -0600

    add(DynamoDBAuthDAO) createAuth functionality

--- tweeter-server/src/dao/auth/DynamoDBAuthDAO.ts
+++ tweeter-server/src/dao/auth/DynamoDBAuthDAO.ts
@@ -1,4 +1,4 @@
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { AuthToken } from "tweeter-shared";
 import { AuthDAO } from "./AuthDAO";
 
@@ -13,9 +13,18 @@ export class DynamoDBAuthDAO implements AuthDAO {
   public constructor(client: DynamoDBDocumentClient) {
     this.client = client;
   }
-  createAuth(authToken: AuthToken, userAlias: string): void {
-    throw new Error("Method not implemented.");
+  async createAuth(authToken: AuthToken, userAlias: string): Promise<void> {
+    const params = {
+      TableName: this.tableName,
+      Item: {
+        [this.tokenAttr]: authToken.token,
+        [this.timestampAttr]: authToken.timestamp,
+        [this.aliasAttr]: userAlias,
+      },
+    };
+    await this.client.send(new PutCommand(params));
   }
+
   getAuth(token: string): [authToken: AuthToken, userAlias: string] {
     throw new Error("Method not implemented.");
   }
