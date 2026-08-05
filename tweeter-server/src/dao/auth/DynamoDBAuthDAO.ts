81ecafd1c2796762fa6c09026241b784a675b3d9
Tristan Weech <twminebro@gmail.com>
2026-08-05 09:03:50 -0600

    add(DynamoDBAuthDAO) attribute variables

--- tweeter-server/src/dao/auth/DynamoDBAuthDAO.ts
+++ tweeter-server/src/dao/auth/DynamoDBAuthDAO.ts
@@ -3,6 +3,11 @@ import { AuthToken } from "tweeter-shared";
 import { AuthDAO } from "./AuthDAO";
 
 export class DynamoDBAuthDAO implements AuthDAO {
+  private readonly tableName = "auth";
+  private readonly tokenAttr = "token";
+  private readonly timestampAttr = "timestamp";
+  private readonly aliasAttr = "alias";
+
   private readonly client: DynamoDBDocumentClient;
 
   public constructor(client: DynamoDBDocumentClient) {
