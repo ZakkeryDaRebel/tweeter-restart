23922cdcc5c15921d43c2a7c74a2861e3561c710
Tristan Weech <twminebro@gmail.com>
2026-08-05 08:40:04 -0600

    add(DynamoDBUserDAO) function calls to DynamoDB for get and create user

--- tweeter-server/src/dao/user/DynamoDBUserDAO.ts
+++ tweeter-server/src/dao/user/DynamoDBUserDAO.ts
@@ -1,3 +1,8 @@
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
 import { User } from "tweeter-shared";
 import { UserDAO } from "./UserDAO";
 
@@ -11,12 +16,69 @@ import { UserDAO } from "./UserDAO";
  */
 
 export class DynamoDBUserDAO implements UserDAO {
-  createUser(user: User): void {
-    //code
+  private readonly tableName = "user";
+  private readonly aliasAttr = "alias";
+  private readonly firstNameAttr = "firstName";
+  private readonly lastNameAttr = "lastName";
+  private readonly passwordAttr = "password";
+  private readonly imageUrlAttr = "imageUrl";
+  private readonly followeeCountAttr = "followeeCount";
+  private readonly followerCountAttr = "followerCount";
+
+  private readonly client: DynamoDBDocumentClient;
+
+  public constructor(client: DynamoDBDocumentClient) {
+    this.client = client;
   }
 
-  getUser(alias: string): User {
-    //code
-    return new User("", "", "", "");
+  async createUser(user: User, hashedPassword: string): Promise<void> {
+    const params = {
+      TableName: this.tableName,
+      Item: {
+        [this.aliasAttr]: user.alias,
+        [this.firstNameAttr]: user.firstName,
+        [this.lastNameAttr]: user.lastName,
+        [this.passwordAttr]: hashedPassword,
+        [this.imageUrlAttr]: user.imageUrl,
+        [this.followeeCountAttr]: 0,
+        [this.followerCountAttr]: 0,
+      },
+    };
+    await this.client.send(new PutCommand(params));
+  }
+
+  async getUser(
+    alias: string,
+  ): Promise<
+    [
+      user: User,
+      hashedPassword: string,
+      followeeCount: number,
+      followerCount: number,
+    ]
+  > {
+    const params = {
+      TableName: this.tableName,
+      Key: {
+        [this.aliasAttr]: alias,
+      },
+    };
+    const output = await this.client.send(new GetCommand(params));
+    if (output.Item === undefined) {
+      // no such user. Throw error or return null?
+      throw new Error("No such user");
+    }
+    const user = new User(
+      output.Item[this.aliasAttr],
+      output.Item[this.lastNameAttr],
+      output.Item[this.aliasAttr],
+      output.Item[this.imageUrlAttr],
+    );
+    return [
+      user,
+      output.Item[this.passwordAttr],
+      output.Item[this.followeeCountAttr],
+      output.Item[this.followerCountAttr],
+    ];
   }
 }
