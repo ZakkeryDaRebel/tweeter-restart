ea6366b96e38ef1091442ce97e5e7f88d6437467
Tristan Weech <twminebro@gmail.com>
2026-08-06 12:41:26 -0600

    add(DynamoStoryDAO) createStory functionality

--- tweeter-server/src/dao/story/DynamoStoryDAO.ts
+++ tweeter-server/src/dao/story/DynamoStoryDAO.ts
@@ -1,4 +1,4 @@
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { Status } from "tweeter-shared";
 import { StoryDAO } from "./StoryDAO";
 
@@ -14,9 +14,18 @@ export class DynamoStoryDAO implements StoryDAO {
     this.client = client;
   }
 
-  createStory(post: Status): void {
-    throw new Error("Method not implemented.");
+  async createStory(post: Status): Promise<void> {
+    const params = {
+      TableName: this.tableName,
+      Item: {
+        [this.aliasAttr]: post.user.alias,
+        [this.timestampAttr]: post.timestamp,
+        [this.postAttr]: post.post,
+      },
+    };
+    await this.client.send(new PutCommand(params));
   }
+
   getStory(postAlias: string, timestamp: number): Status {
     throw new Error("Method not implemented.");
   }
